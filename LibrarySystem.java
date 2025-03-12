8cc607930869ff83160afee2a8c0eec36a6598d8
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-12 10:12:16 +0530

    [Sehajpreet] Add: Design an Employee class

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -28,8 +28,8 @@ class Book {
 // Main class
 public class LibrarySystem {
     public static void main(String[] args) {
-        Book book1 = new Book("978-3-16-148410-0", "Java Programming", "RK Singh");
-        Book book2 = new Book("978-0-262-13472-9", "Data Structures", "SP Shukla");
+        Book book1 = new Book("978-3-16-148410-0", "Java Programming", "Sehaj");
+        Book book2 = new Book("978-0-262-13472-9", "Data Structures", "Sehajpreet");
 
         book1.displayBookDetails();
         book2.displayBookDetails();
